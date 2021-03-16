b896f0ecf7039959e39e01a135e71022dadca7db
PhongVuong <phong.vuong@thlsoft.com>
2021-03-16 17:40:44 +0700

    update source

--- Dapper.Core/Entities/Product.cs
+++ Dapper.Core/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +7,19 @@ namespace Dapper.Core.Entities
 {
     public class Product
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("description")]
         public string Description { get; set; }
+        [JsonProperty("barcode")]
         public string Barcode { get; set; }
+        [JsonProperty("rate")]
         public decimal Rate { get; set; }
+        [JsonProperty("added_on")]
         public DateTime AddedOn { get; set; }
+        [JsonProperty("modified_on")]
         public DateTime ModifiedOn { get; set; }
     }
 }
